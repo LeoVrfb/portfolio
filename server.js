26479c6cef30b075d12b767deb3ea616bb73090d
Leo Hengebaert <lnettoyeur75@gmail.com>
2024-06-25 23:06:31 +0200

    fixing pb with cors policy

--- server.js
+++ server.js
@@ -26,6 +26,22 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+const origin = req.headers.origin;
+if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+}
+
+if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+}
+
 
 // Middleware pour parser les données POST
 app.use(urlencoded({ extended: true }));
