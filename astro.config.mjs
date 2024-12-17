561598c72f007b23534e13688aa50a4d2c95f358
lhengebaertArtefact <leo.hengebaert@artefact.com>
2024-12-17 21:59:30 +0100

    updating li icons using mdx js react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), mdx()]
   // autres configurations
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const work = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
+    url: z.string().optional(),
     publishDate: z.date(),
     tags: z.array(z.string()),
     img: z.string().optional(),
